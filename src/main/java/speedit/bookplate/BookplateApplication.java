8a0f7bd15058b5e2576a6a3f65551bc0bf59e8c0
HyungJooKim <hyungjookim12@gmail.com>
2022-06-11 14:12:41 +0900

    [:wrench: config] 프로젝트 초기 설정 및 mysql 연동
    1. 패키지 파일 추가
    2. application.yml 파일 추가
    3. docker-compose.yml 추가
    4. gradle 구성 파일 추가

---
+++ src/main/java/speedit/bookplate/BookplateApplication.java
@@ -0,0 +1,13 @@
+package speedit.bookplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/speedit/bookplate/BookplateApplicationTests.java
@@ -0,0 +1,13 @@
+package speedit.bookplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
