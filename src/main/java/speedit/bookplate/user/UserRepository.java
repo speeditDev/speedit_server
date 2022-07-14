129d9c1d5ff09b626abb404b84ec44c53a6f39e9
eunyoung23 <eun970923@naver.com>
2022-07-14 21:58:24 +0900

    [:sparkles: feat] 회원 탈퇴 api 구현

--- src/main/java/speedit/bookplate/user/UserRepository.java
+++ src/main/java/speedit/bookplate/user/UserRepository.java
@@ -1,15 +1,25 @@
 package speedit.bookplate.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import speedit.bookplate.user.dto.UserDto;
 import speedit.bookplate.user.entity.User;
+import speedit.bookplate.user.entity.enumTypes.UserStatus;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
     Optional<User> findByNickname(String nickname);
+
     boolean existsByNickname(String name);
+
     User findByUserIdx(Long userIdx);
+
+    @Modifying
+    @Query("UPDATE User u SET u.status=:status WHERE u.userIdx=:userIdx")
+    void updateInactiveUser(Long userIdx, UserStatus status);
 }
