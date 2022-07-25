9ad799db00b4b7564b792be8f9a7524af46fe17e
eunyoung23 <eun970923@naver.com>
2022-07-26 00:28:29 +0900

    [:bug: fix] scrap 오류 나는 부분 주석 처리

--- src/main/java/speedit/bookplate/scrap/ScrapController.java
+++ src/main/java/speedit/bookplate/scrap/ScrapController.java
@@ -1,31 +1,8 @@
 package speedit.bookplate.scrap;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import speedit.bookplate.book.BookService;
-import speedit.bookplate.book.dto.GetDetailResDto;
-import speedit.bookplate.book.dto.SearchBookResDto;
-import speedit.bookplate.book.dto.StorageBookResDto;
-import speedit.bookplate.book.entity.Book;
-import speedit.bookplate.book.repository.BookRepository;
-import speedit.bookplate.config.BaseResponse;
-import speedit.bookplate.feed.dto.AmdFeedReqDto;
-import speedit.bookplate.feed.dto.AmdFeedResDto;
-import speedit.bookplate.feed.entity.Feed;
-import speedit.bookplate.scrap.dto.CancelScrapReqDto;
-import speedit.bookplate.scrap.dto.CancelScrapResDto;
-import speedit.bookplate.scrap.dto.ScrapResDto;
-import speedit.bookplate.scrap.entity.Scrap;
-import speedit.bookplate.scrap.repository.ScrapRepository;
-
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Api(tags = {"Scrap"})
 @RequiredArgsConstructor
@@ -33,6 +10,7 @@ import java.util.List;
 @RequestMapping
 public class ScrapController {
 
+    /*
     private final ScrapService scrapService;
 
     @ApiOperation(value = "피드 스크랩하기", notes = "특정한 피드를 스크랩한다.")
@@ -48,5 +26,5 @@ public class ScrapController {
         Scrap canceledScrapFeed = scrapService.cancelScrap(cancelScrapReqDto);
         return new BaseResponse<Object>(new CancelScrapResDto(canceledScrapFeed.getFeed().getFeedIdx(),
                 canceledScrapFeed.getStatus(), canceledScrapFeed.getCreatedAt(), canceledScrapFeed.getUpdatedAt()));
-    }
+    }*/
 }
